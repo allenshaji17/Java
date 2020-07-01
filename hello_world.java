56a88f8aecfa3407c102665a1b696028fa5c0414
allenshaji17 <52339640+allenshaji17@users.noreply.github.com>
2020-07-01 17:48:05 +0530

    Created Hello_world.java
    This is a simple program to print out "Hello world!"

---
+++ hello_world.java
@@ -0,0 +1,15 @@
+package hello_world;
+
+/**
+ *
+ * @author allen
+ */
+public class Hello_world {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
