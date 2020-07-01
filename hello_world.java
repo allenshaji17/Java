bdb0915a3e36aafc1429db514c2d666096602bff
allenshaji17 <52339640+allenshaji17@users.noreply.github.com>
2020-07-01 17:48:57 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,14 +1,5 @@
 package hello_world;
-
-/**
- *
- * @author allen
- */
 public class Hello_world {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
